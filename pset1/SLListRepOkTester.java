9324da088abb2e9c6e4a17488181002a8d5e20c9
Patrick Sigourney <psigourney@gmail.com>
2018-06-17 12:43:15 -0500

    1.1, 1.2 complete 1.3 started

---
+++ pset1/SLListRepOkTester.java
@@ -0,0 +1,22 @@
+package pset1;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import pset1.SLList.Node;
+
+public class SLListRepOkTester {
+    @Test
+    public void t0(){
+        pset1.SLList l = new pset1.SLList();
+        assertTrue(l.repOk());
+
+    }
+
+    @Test
+    public void t1(){
+        pset1.SLList l = new pset1.SLList();
+        Node n = new Node();
+
+    }
+}
