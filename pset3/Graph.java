68fc8a02ccbc1e548de83e66e2a1c3bd171e24d1
Patrick Sigourney <psigourney@gmail.com>
2018-07-08 08:15:18 -0500

    created pset3

---
+++ pset3/Graph.java
@@ -0,0 +1,44 @@
+package pset3;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class Graph {
+    private int numNodes;
+    private boolean[][] edges;
+
+    public Graph(int size){
+        numNodes = size;
+
+
+        //Your code goes here
+    }
+
+    public String toString(){
+        return "numNodes: " + numNodes + "\n" + "edges: " + Arrays.deepToString(edges);
+    }
+
+    public boolean equals(Object o){
+        if(o.getClass() != Graph.class) return false;
+        return toString().equals(o.toString());
+    }
+
+    public void addEdge(int from, int to){
+        //postcondition: adds a directed edge "from" -> "to" to this graph
+
+        //Your code goes here
+    }
+
+    public boolean reachable(Set<Integer> sources, Set<Integer> targets){
+        if(sources == null || targets == null) throw new IllegalArgumentException();
+
+        //postcondition: returns true if
+        //      1) "sources" does not contain an illegal node
+        //      2) "targets" dies not contain an illegal node
+        //      3) for each node 'm' in set "targets", there is some node 'n' in set "sources" such
+        //      that there is a directed path that starts at "n" in "this"; and false otherwise.
+
+
+        //Your code goes here
+    }
+}
