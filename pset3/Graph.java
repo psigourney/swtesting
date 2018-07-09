00938aafee2693d7820cc3152b2b6f0ca020d649
Patrick Sigourney <psigourney@gmail.com>
2018-07-09 15:50:25 -0500

    Completed reachable()

--- pset3/Graph.java
+++ pset3/Graph.java
@@ -1,7 +1,6 @@
 package pset3;
 
-import java.util.Arrays;
-import java.util.Set;
+import java.util.*;
 
 public class Graph {
     private int numNodes;
@@ -25,8 +24,8 @@ public class Graph {
         edges[from][to] = true;
     }
 
-    public boolean reachable(Set<Integer> sources, Set<Integer> targets){
-        if(sources == null || targets == null) throw new IllegalArgumentException();
+    public boolean reachable(Set<Integer> sources, Set<Integer> targets) {
+        if (sources == null || targets == null) throw new IllegalArgumentException();
 
         //postcondition: returns true if
         //      1) "sources" does not contain an illegal node
@@ -34,8 +33,76 @@ public class Graph {
         //      3) for each node 'm' in set "targets", there is some node 'n' in set "sources" such
         //      that there is a directed path that starts at "n" in "this"; and false otherwise.
 
+        for( Integer targetNode : targets ){
+            if(targetNode < 0 || targetNode >= numNodes) return false;
+            boolean targetFound = false;
 
-        //Your code goes here (1.3)
+            for( Integer sourceNode : sources ){
+                if(sourceNode < 0 || sourceNode >= numNodes) return false;
+                //System.out.println("Checking isReachable(" + sourceNode + ", " + targetNode + ")");
+                if(isReachable(sourceNode, targetNode)) {
+                    //System.out.println("True: isReachable(" + sourceNode + ", " + targetNode + ")");
+                    targetFound = true;
+                }
+            }
+            if(!targetFound) return false; //One of the targets can't be reached
+        }
         return true;
     }
+
+    //isReachable Code Copied from: https://www.geeksforgeeks.org/find-if-there-is-a-path-between-two-vertices-in-a-given-graph/
+    public boolean isReachable(int s, int d)
+    {
+        if(s==d) return true;
+
+        // Mark all the vertices as not visited(By default set as false)
+        boolean visited[] = new boolean[numNodes];
+
+        // Create a queue for BFS
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Mark the current node as visited and enqueue it
+        visited[s]=true;
+        queue.add(s);
+
+        // 'i' will be used to get all adjacent vertices of a vertex
+        Iterator<Integer> i;
+
+        while (queue.size()!=0)
+        {
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+
+            int n;
+
+            //Create list of adjacent nodes
+            LinkedList<Integer> adjList = new LinkedList<>();
+            for(int counter=0; counter < numNodes; counter++){
+                if(edges[s][counter]) adjList.add(counter);
+            }
+
+            i = adjList.listIterator();
+
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            while (i.hasNext()){
+                n = i.next();
+
+                // If this adjacent node is the destination node,
+                // then return true
+                if (n==d) return true;
+
+                // Else, continue to do BFS
+                if (!visited[n]){
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+        // If BFS is complete without visited d
+        return false;
+    }
+
+
 }
--- pset3/GraphTester.java
+++ pset3/GraphTester.java
@@ -1,6 +1,10 @@
 package pset3;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphTester {
@@ -16,4 +20,28 @@ public class GraphTester {
 
     //Create tests for reachable
 
+    @Test
+    public void tae99(){
+        Graph g = new Graph(5);
+        g.addEdge(0,2);
+        g.addEdge(2,1);
+        g.addEdge(2,4);
+        g.addEdge(2,3);
+        g.addEdge(1,4);
+        g.addEdge(1,2);
+
+        Set<Integer> sources = new HashSet<Integer>();
+        Set<Integer> targets = new HashSet<Integer>();
+
+        sources.add(0);
+        sources.add(1);
+        //sources.add(-1);
+        targets.add(0);
+        targets.add(1);
+        //targets.add(6);
+        targets.add(3);
+        targets.add(4);
+
+        assertTrue(g.reachable(sources, targets));
+    }
 }
