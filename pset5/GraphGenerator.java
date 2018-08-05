333e4345b994dd23ee22de70cb1d7b8d7a0e9de7
Patrick Sigourney <psigourney@gmail.com>
2018-08-05 09:58:40 -0500

    Reworked question 1; question 2 planned out

--- pset5/GraphGenerator.java
+++ pset5/GraphGenerator.java
@@ -13,11 +13,8 @@ public class GraphGenerator {
     public CFG createCFG(String className) throws ClassNotFoundException{
         CFG cfg = new CFG();
         JavaClass jc = Repository.lookupClass(className);
-        //System.out.println("JavaClass jc: \n" + jc);
         ClassGen cg = new ClassGen(jc);
-        //System.out.println("ClassGen cg: \n" + cg);
         ConstantPoolGen cpg = cg.getConstantPool();
-        //System.out.println("ConstantPoolGen cpg: \n" + cpg);
 
         for(Method m : cg.getMethods()){
             System.out.println("Method: " + m);
@@ -26,12 +23,11 @@ public class GraphGenerator {
             InstructionHandle[] handles = il.getInstructionHandles();
 
             //Create dummy exit node
-            int finalInstrPos = (handles[handles.length-1].getPosition());
-            int exitNodePos = (handles[handles.length-1].getPosition())+1;
+            int exitNodePos = -1;
             cfg.addNode(exitNodePos, m, jc);
 
-            int prevNodePos = -1;
             for(InstructionHandle ih : handles){
+                System.out.println("IH: " + ih + " Prev: " + ih.getPrev() + " Next: " + ih.getNext());
                 int position = ih.getPosition();
                 cfg.addNode(position, m, jc);
                 Instruction inst = ih.getInstruction();
@@ -41,39 +37,74 @@ public class GraphGenerator {
                     BranchInstruction brInst = (BranchInstruction) ih.getInstruction();
                     cfg.addEdge(position, brInst.getTarget().getPosition(), m, jc);
                 }
+
                 //If instruction is an exit point, create edge to exit node
-                if(inst.getName() == "ireturn"){
+                //else create edge to next instruction
+                if(ih.getNext() == null || inst.getName() == "ireturn"){
                     cfg.addEdge(position, exitNodePos, m, jc);
                 }
-                //If instruction is last instruction in list, create edge to exit node
-                if(position == finalInstrPos){
-                    cfg.addEdge(position, exitNodePos, m, jc);
+                else{
+                    cfg.addEdge(position, ih.getNext().getPosition(), m, jc);
                 }
-                //If previous instruction should have an edge to current instruction
-                if(prevNodePos != -1){
-                    cfg.addEdge(prevNodePos, position, m, jc);
-                }
-                //If current instruction is a return, the next instruction won't have an edge from it
-                if(inst.getName() == "ireturn"){
-                    prevNodePos = -1;
-                } else { prevNodePos = position; }
             }
         }
         return cfg;
     }
 
     public CFG createCFGWithMethodInvocation(String className) throws ClassNotFoundException{
-        //TODO: Your code goes here
+        CFG cfg = new CFG();
+        JavaClass jc = Repository.lookupClass(className);
+        ClassGen cg = new ClassGen(jc);
+        ConstantPoolGen cpg = cg.getConstantPool();
 
+        for(Method m : cg.getMethods()){
+            System.out.println("Method: " + m);
+            MethodGen mg = new MethodGen(m, cg.getClassName(), cpg);
+            InstructionList il = mg.getInstructionList();
+            InstructionHandle[] handles = il.getInstructionHandles();
 
-        //Dummy results
-        CFG result = new CFG();
-        return result;
+            //Create dummy exit node
+            int exitNodePos = -1;
+            cfg.addNode(exitNodePos, m, jc);
+
+            for(InstructionHandle ih : handles){
+                System.out.println("IH: " + ih + " Prev: " + ih.getPrev() + " Next: " + ih.getNext());
+                int position = ih.getPosition();
+                cfg.addNode(position, m, jc);
+                Instruction inst = ih.getInstruction();
+
+                //If instruction is a conditional:
+                if(inst instanceof BranchInstruction){
+                    BranchInstruction brInst = (BranchInstruction) ih.getInstruction();
+                    cfg.addEdge(position, brInst.getTarget().getPosition(), m, jc);
+                }
+
+                //If instruction is a method call:
+                if(inst instanceof InvokeInstruction) {
+                    InvokeInstruction invInst = (InvokeInstruction) inst;
+                    System.out.println("    InvokeInstMethod: " + invInst.getMethodName(cpg));
+
+                    // Create edge to:  => .getMethodName(cpg), position 0, .getClassName(cpg)
+                    // Create edge from: .getMethodName, position -1, .getClassName ==> .getNext
+                }
+
+                //If instruction is an exit point, create edge to exit node
+                //else create edge to next instruction
+                if(ih.getNext() == null || inst.getName() == "ireturn"){
+                    cfg.addEdge(position, exitNodePos, m, jc);
+                }
+                else{
+                    cfg.addEdge(position, ih.getNext().getPosition(), m, jc);
+                }
+            }
+        }
+        return cfg;
     }
 
     public static void main(String[] a) throws ClassNotFoundException{
         GraphGenerator gg = new GraphGenerator();
-        CFG mycfg = gg.createCFG("pset5.C");    //Example invocation of createCFG
+        CFG mycfg = gg.createCFG("pset5.C");
+        //CFG mycfg = gg.createCFGWithMethodInvocation("pset5.D");
 
         System.out.println(mycfg);
         Iterator<Map.Entry<CFG.Node, Set<CFG.Node>>> itr = mycfg.edges.entrySet().iterator();
@@ -82,7 +113,5 @@ public class GraphGenerator {
             System.out.println("SourceNode: " + entry.getKey().method + ": " + entry.getKey().position + "; DestNodes: " + entry.getValue());
         }
 
-
-        //gg.createCFGWithMethodInvocation("pset5.D");    //Example invocation of createCFGWith....
     }
 }
